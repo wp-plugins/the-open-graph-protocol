94184ad469acc4c541b75eee7bc09d07f33e22a9
albertochoa <albertochoa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-14 01:19:42 +0000

    First version 0.1
    git-svn-id: https://plugins.svn.wordpress.org/the-open-graph-protocol/trunk@475108 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ open-graph-protocol.php
@@ -0,0 +1,234 @@
+<?php
+/**
+ * Plugin Name: The Open Graph Protocol
+ * Plugin URI: http://niftytheme.com
+ * Description: The Open Graph protocol enables any web page to become a rich object in a social graph.
+ * Version: 0.1
+ * Author: Luis Alberto Ochoa Esparza
+ * Author URI: http://luisalberto.org
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation. You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if not, write
+ * to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * @package Open Graph Protocol
+ * @version 0.1
+ * @author Luis Alberto Ochoa Esparza <soy@luisalberto.org>
+ * @copyright Copyright (C) 2011, Luis Alberto Ochoa Esparza
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+/* Add Open Graph Metadata to the <head> area. */
+add_action( 'wp_head', 'ogp_head_metadata', 1 );
+
+/**
+ * Display the HTML for the OGP metadata.
+ *
+ * @since 0.1
+ */
+function ogp_head_metadata() {
+
+	/* Set some variables for use within the function */
+	$meta     = array();
+	$metadata = ogp_get_metadata();
+
+	/* Create the HTML for the OGP metadata */
+	foreach( $metadata as $property => $content )
+		$meta[] = "<meta property='og:{$property}' content='{$content}' />";
+
+	echo apply_filters( 'ogp_head_metadata', join( PHP_EOL, $meta ) );
+}
+
+/**
+ * Generates the content metadata "og:$property" based on object.
+ *
+ * Users or developers can overwritten using the "ogp_get_metadata" filter hook.
+ *
+ * @since 0.1
+ */
+function ogp_get_metadata() {
+	global $wp_query;
+
+	/* Set some variables for use within the function */
+	$metadata = array();
+	$object   = $wp_query->get_queried_object();
+
+	/* Front page of the site */
+	if ( is_front_page() && !is_paged() ) {
+
+		$metadata = apply_filters( 'ogp_get_home_metadata', array(
+			'type' => 'website'
+		) );
+	}
+
+	/* Blog page */
+	if ( is_home() ) {
+
+		$metadata = apply_filters( 'ogp_get_blog_metadata', array(
+			'type' => 'blog'
+		) );
+	}
+
+	/* Singular views */
+	elseif ( is_singular() ) {
+
+		$metadata = apply_filters( "ogp_get_{$object->post_type}_metadata", array(
+			'title'          => get_the_title(),
+			'type'           => 'article',
+			'url'            => get_permalink(),
+			'image'          => ogp_get_the_image_post( $object->ID ),
+			'published_time' => get_post_time( 'l, F jS, Y, g:i a' ),
+			'modified_time'  => get_the_modified_time( 'l, F jS, Y, g:i a' ),
+			'author'         => get_the_author_meta( 'display_name', $object->post_author )
+		) );
+	}
+
+	return apply_filters( 'ogp_get_metadata', $metadata, $object );
+}
+
+/**
+ * The main function for retrieving an image into a post.
+ *
+ * @since 0.1
+ * @param int $post_id
+ * @return string Image URL
+ */
+function ogp_get_the_image_post( $post_id ) {
+
+	/* Set $post_type variable for use within the cache */
+	$post_type = get_post_type( $post_id );
+
+	/* Get cache key based on $post_id and $post_type */
+	$key = md5( serialize( array( 'post_id' => $post_id, 'post_type' => $post_type ) ) );
+
+	/* Check for a cached image */
+	$image_cache = wp_cache_get( $post_id, 'ogp_get_the_image_post' );
+
+	if ( !is_array( $image_cache ) )
+		$image_cache = array();
+
+	/* If there is no cached image, let's see if one exists */
+	if ( !isset( $image_cache[$key] ) ) {
+
+		/* Check for a post image (WP feature) */
+		$image = ogp_get_image_by_post_thumbnail( $post_id );
+
+		/* If no image found, check for a post image (WP feature) */
+		if ( empty( $image ) )
+			$image = ogp_get_the_image_by_attachment( $post_id );
+
+		/* If no image found, scan the post for images */
+		if ( empty( $image ) )
+			$image = ogp_get_the_image_by_scan( $post_id );
+
+		/* If no image found, get the default image */
+		if ( empty( $image ) )
+			$image = ogp_get_the_image_by_default();
+
+		/* Set the image cache for the specific post. */
+		$image_cache[$key] = $image;
+		wp_cache_set( $post_id, $image_cache, 'ogp_get_the_image_post' );
+	}
+
+	/* If an image was already cached for the post and arguments, use it */
+	else {
+		$image = $image_cache[$key];
+	}
+
+	return $image;
+}
+
+/**
+ * Check if a post has a Post Thumbnail attached and return it.
+ * If no post image ID is found, return false.
+ *
+ * @since 0.1
+ * @param int $post_id
+ * @return string|bool Image URL. False if no image is found
+ */
+function ogp_get_image_by_post_thumbnail( $post_id ) {
+
+	/* Check for a post image ID */
+	$post_thumbnail_id = get_post_thumbnail_id( $post_id );
+
+	/* If no post image ID is found, return default image */
+	if ( empty( $post_thumbnail_id ) )
+		return false;
+
+	/* Get the attachment image source, this should return an array */
+	$image = wp_get_attachment_image_src( $post_thumbnail_id, 'thumbnail' );
+
+	/* Return the image URL */
+	return $image[0];
+}
+
+/**
+ * Check for attachment images.
+ *
+ * @since 0.1
+ * @param int $post_id
+ * @return string|bool Image URL. False if no image is found
+ */
+function ogp_get_the_image_by_attachment( $post_id ) {
+
+	/* Set $attachment_id variable */
+	$attachment_id = 0;
+
+	/* Get the post type of the current post. */
+	$post_type = get_post_type( $post_id );
+
+	/* Check if the post itself is an image attachment. */
+	if ( 'attachment' == $post_type && wp_attachment_is_image( $post_id ) ) {
+		$attachment_id = $post_id;
+	}
+
+	/* Check if we have an attachment ID before proceeding. */
+	if ( !empty( $attachment_id ) ) {
+
+		/* Get the attachment image. */
+		$image = wp_get_attachment_image_src( $attachment_id, 'thumbnail' );
+
+		/* Return the image URL. */
+		return $image[0];
+	}
+
+	/* Return false for anything else. */
+	return false;
+}
+
+/**
+ * Scans the post for images within the content. Shouldn't use if using large images
+ * within posts, better to use the other options.
+ *
+ * @since 0.1
+ * @param int $post_id
+ * @return string|bool Image URL. False if no image is found
+ */
+function ogp_get_the_image_by_scan( $post_id ) {
+
+	/* Search the post's content for the <img /> tag and get its URL. */
+	preg_match_all( '|<img.*?src=[\'"](.*?)[\'"].*?>|i', get_post_field( 'post_content', $post_id ), $matches );
+
+	/* If there is a match for the image, return its URL. */
+	if ( isset( $matches ) && !empty( $matches[1][0] ) )
+		return $matches[1][0];
+
+	return false;
+}
+
+/**
+ * The function simply returns the image URL.
+ *
+ * @since 0.1
+ * @return string Image URL
+ */
+function ogp_get_the_image_by_default() {
+	return apply_filters( 'ogp_get_the_image_by_default', plugin_dir_url ( __FILE__ ) . 'default.png' );
+}
+
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== The Open Graph Protocol ===
+Contributors: albertochoa
+Donate link: http://niftytheme.com
+Tags: facebook, google plus, open graph, social graph, protocol
+Requires at least: 3.0
+Tested up to: 3.3
+Stable tag: 0.1
+
+The Open Graph protocol enables any web page to become a rich object in a social graph.
+
+== Description ==
+
+The *Open Graph Protocol* enables any web page to become a rich object in a social graph. For instance, this is used on Facebook to allow any web page to have the same functionality as any other object on Facebook.
+
+While many different technologies and schemas exist and could be combined together, there isn't a single technology which provides enough information to richly represent any web page within the social graph. The Open Graph protocol builds on these existing technologies and gives developers one thing to implement. Developer simplicity is a key goal of the Open Graph protocol which has informed many of the technical design decisions.
+
+= Documentation =
+* [Understanding the design decisions](http://www.scribd.com/doc/30715288/The-Open-Graph-Protocol-Design-Decisions)
+* [Facebook Developers](http://developers.facebook.com/docs/opengraph/)
+
+= Recommendations =
+* [ShareThis](http://sharethis.com/)
+* [Lockerz Share](http://share.lockerz.com/)
+
+== Installation ==
+
+1. Unzip the open-graph-protocol.zip folder
+1. Upload the open-graph-protocol folder to your /wp-content/plugins directory
+1. In your WordPress dashboard, head over to the Plugins section
+1. Activate Open Graph Protocol
+
+== Changelog ==
+
+= 0.1 =
+* First public release
